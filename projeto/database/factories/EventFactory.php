f8154cf4ccf7eb33c52ae5ef27da02bc55de0001
Cesar Faustino <csarfau@gmail.com>
2024-06-26 17:57:46 -0300

    [fix]: add EventSeeder and EventFactory

--- projeto/database/factories/EventFactory.php
+++ projeto/database/factories/EventFactory.php
@@ -17,7 +17,13 @@ class EventFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'title' => $this->faker->unique()->sentence(),
+            'description' => $this->faker->paragraph(),
+            'init_date' => $this->faker->dateTimeThisDecade,
+            'end_date' => $this->faker->dateTimeThisDecade,
+            'max_participants' => $this->faker->randomNumber(2),
+            'entry_price' => $this->faker->randomNumber(2),
+            'event_image' => $this->faker->imageUrl(400, 400)
         ];
     }
 }
