78255081631b2373a145794011025f3182714ff4
Cesar Faustino <csarfau@gmail.com>
2024-06-27 09:32:17 -0300

    [style]: add some style to register page

--- projeto/resources/views/register.blade.php
+++ projeto/resources/views/register.blade.php
@@ -1,15 +1,33 @@
 @extends('layout')
 @section('title', 'Página de Cadastro')
+@section('body-class', 'custom-body-class')
+@push('styles')
+<style>
+    .custom-body-class {
+        height: 100vh;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+</style>
+@endpush
+
 @section('content')
 
     <div>
-        <form method="POST" action="./register">
+        <form class="container p-3 rounded text-center bg-secondary" method="POST" action="{{ route('register') }}">
             @csrf
-
-            <input name="name" type="text" placeholder="Nome" required><br>
-            <input name="email" type="email" placeholder="E-mail" required><br>
-            <input name="password" type="password" placeholder="Senha" required><br>
-            <button type="submit">Cadastrar</button>
+            <legend class="text-light">Cadastro</legend>
+            <div class="mb-3">
+                <input class="form-control" name="name" type="text" placeholder="Nome" required>
+            </div>
+            <div class="mb-3">
+                <input class="form-control" name="email" type="email" placeholder="E-mail" required>
+            </div>
+            <div class="mb-3">
+                <input class="form-control" name="password" type="password" placeholder="Senha" required>
+            </div>
+            <button type="submit" class="btn btn-primary">Cadastrar</button>
         </form>
     </div>
 
